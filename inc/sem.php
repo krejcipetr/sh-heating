e444b01e3ad37ca843f7d17bf5a392a9bcd4924c
krejci <krejci@pevnypc.home.it-g.cz>
2017-12-08 19:43:30 +0100

    U pro primy pristup pres expect a uprava komunikace se spinaci.

--- inc/sem.php
+++ inc/sem.php
@@ -1,16 +1,25 @@
 <?php
 
+/**
+ * @return integer
+ */
 function semget () {
 	! file_exists ( '/tmp/heating' ) && touch ( "/tmp/heating" ) && chmod ( '/tmp/heating', 0777 );
 	$l_id = ftok ( "/tmp/heating", "P" );
 	return sem_get ( $l_id, 1 );
 }
 
+/**
+ *
+ */
 function semdown () {
 	$GLOBALS ['sem'] = semget ();
 	sem_acquire ( $GLOBALS ['sem'] );
 }
 
+/**
+ *
+ */
 function semup () {
 	sem_release ( $GLOBALS ['sem'] );
 }
