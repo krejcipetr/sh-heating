6c09113f4063b62ea7390afd4b120b199de730ab
krejcipetr <petr.krejci@it-g.cz>
2017-12-18 09:46:30 +0100

    S jednoceni formatovani a prava zapnuti kotlu

--- heatsource_off.php
+++ heatsource_off.php
@@ -6,6 +6,7 @@ require_once 'inc/bt04a.php';
 
 foreach ($GLOBALS['heating']['sources'] as $l_source) {
     bt04a_off($l_source['dev']);
+    sleep (10);
     printf("%s=%s" . PHP_EOL, $l_source['name'],
             (bt04a_getstate($l_source['dev'])) ? "On" : "Off");
 }
--- heatsource_on.php
+++ heatsource_on.php
@@ -5,7 +5,8 @@ require_once 'config.php';
 require_once 'inc/bt04a.php';
 
 foreach ($GLOBALS['heating']['sources'] as $l_source) {
-    bt04a_off($l_source['dev']);
+    bt04a_on($l_source['dev']);
+    sleep(10);
     printf("%s=%s" . PHP_EOL, $l_source['name'],
             (bt04a_getstate($l_source['dev'])) ? "On" : "Off");
 }
