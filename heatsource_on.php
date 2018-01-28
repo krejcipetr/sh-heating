350693ae105230181ccfb67d13f9a4f723c2b4a4
krejci <krejci@pevnypc.home.it-g.cz>
2018-01-28 15:06:05 +0100

    Upraveno na zvlast potvrzeni a zvlast zjisteni stavu

--- heatsource_on.php
+++ heatsource_on.php
@@ -5,7 +5,10 @@ require_once 'config.php';
 require_once 'inc/bt04a.php';
 
 foreach ($GLOBALS['heating']['sources'] as $l_source) {
-    bt04a_off($l_source['dev']);
-    printf("%s=%s" . PHP_EOL, $l_source['name'],
-            (bt04a_getstate($l_source['dev'])) ? "On" : "Off");
+    bt04a_on($l_source['dev']);
+}
+sleep(20);
+foreach ($GLOBALS['heating']['sources'] as $l_source) {
+	printf("%s=%s" . PHP_EOL, $l_source['name'],
+			(bt04a_getstate($l_source['dev'])) ? "On" : "Off");
 }
